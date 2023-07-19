9a724dec2ff852fa91dee77cd9d4634eab8c405e
PsycheeBoi <94575520+PsycheeBoi@users.noreply.github.com>
2023-07-19 00:42:38 -0400

    Added simpleMethods file to clean up coreMethods

--- stockScreener/coreMethods.py
+++ stockScreener/coreMethods.py
@@ -1,7 +1,6 @@
 import yfinance as yf
 import json as js
-
-
+import simpleMethods
 
 # Darin Renusch
 
@@ -43,95 +42,13 @@ def callTickerData(ticker, tickers):
         
         print("This ticker is not loaded! Please modify the settings.json file to call this ticker.")
         
-        
-    """
-        Processes Pandas dataframe data into a workable dict
-        given {data}.
-        
-    """
-
-def processData(data):
-    
-    ret = {}
-    z = 0
-    for k,i,j  in zip(data.index, data['High'], data['Close']):
-        
-        ret[z] = {'date': k, 'high': i, 'close': j}
-        z += 1
-    
-    return ret
-
-"""
-    Calculates SMA for the specified time period {days}
-    given {closePrices} of a particular stock.
-        
-"""
-
-def calculateSMA(closePrices, days):
-    
-    sma = 0
-    
-    for i in range(len(closePrices)):
-        sma += closePrices[i]["close"]
-    
-    return (sma/days)
-
-"""
-    Gets and returns 30 day historical data for {stock}
-        
-"""
-def getThirtyDayData(stock, tickers):
-    
-    yFinanceObj = tickers[stock]
-    return yFinanceObj.history(period="30d")
-    
-"""
-    Gets and returns 10 day historical data for {stock}
-        
-"""
-def getTenDayData(stock, tickers):
-    yFinanceObj = tickers[stock]
-    return yFinanceObj.history(period="10d")
-
-
-"""
-    Uses 30 day and 10 ten day SMAs to 
-    determine a bullish or bearish sentiment
-    on {stock}
-
-"""
-
-def longShortMean(stock, tickers):
-    tenDayData = getTenDayData(stock, tickers)
-    thirtyDayData = getThirtyDayData(stock, tickers)
-    
-    cleanTenDay = processData(tenDayData)
-    cleanThirtyDay = processData(thirtyDayData)
-    
-    tenSma = calculateSMA(cleanTenDay, 10)
-    thirtySma = calculateSMA(cleanThirtyDay, 30)
-    
-    percent_difference = (tenSma - thirtySma) / thirtySma
-    
-    if percent_difference > 0.08:
-        print("Go short!")
-        
-    elif percent_difference < -.08:
-        print("Go Long!")
-    else:
-        print("Indifferent")
-    
-    
-    
-    
-    
     
     
 
 def main():
     
     workingTickers = processSettings()
-    longShortMean("MSFT", workingTickers)
+    simpleMethods.longShortMean("MSFT", workingTickers)
     
     
 
