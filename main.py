dff079d53ecc96dd8ce6883e54f79ea26961aa07
DasDog <139926317+DasDog@users.noreply.github.com>
2023-07-20 23:58:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 # Basic Fundamental Stock Screener
 # ---------------------------------------------------------------------------
 
+
 def getPreferences():
     """Loads the JSON file containing the parameters the user wishes to screen stocks for.
 
@@ -17,6 +18,7 @@ def getPreferences():
     return userWants
 
 # ---------------------------------------------------------------------------
+
 
 def getTickers(exchange):
     """Gathers all tickers of stocks we'd like to screen
@@ -65,7 +67,12 @@ def getTickers(exchange):
 
 
 # MAIN --------------------------------------------------------------------------------
-def main(): 
+def main():
+
+    # Create the DataFrame that will store all valid stocks with basic info
+    validStocks = pd.DataFrame(columns=['Ticker', 'Company Name', 'Sector', 'Industry',
+                               'Market Cap', 'Price', 'P/E', 'Dividend Rate', 'Analyst Recommendation'])
+
     # Get the user's preferences they would like to screen for
     userWants = getPreferences()
 
@@ -73,21 +80,26 @@ def main():
     # exchange: NASDAQ, NYSE, or BOTH
     tickerList = getTickers(userWants['exchange'])
 
+    # Check every gathered ticker against the user's preferences
     progressCount = 0
     for ticker in tickerList:
         save = True
-        
+
+        # Prints how far we are in the screening process
         progressCount += 1
         progressPercent = progressCount / len(tickerList) * 100
         progressPercent = round(progressPercent, 2)
         print("Progress: ", progressCount, "/",
               len(tickerList), "... (", progressPercent, "%)")
-        try:
+
+        try:  # Try so if data cannot be grabbed it will skip the stock
             stock = yf.Ticker(ticker)
             data = stock.info
+
+            # Check if the stock meets all of the preferences the user specified
             for key, value in userWants.items():
                 if not (value == ""):
-                    #print(data["symbol"])
+
                     match (key):
 
                         case ('sector'):
@@ -99,7 +111,7 @@ def main():
                             if not (value == data['industry']):
                                 save = False
                                 break
-                            
+
                         case ('dividendRateMin'):
                             if not (float(value) <= data['dividendRate']):
                                 save = False
@@ -119,7 +131,7 @@ def main():
                             if not (float(value) >= data['trailingPE']):
                                 save = False
                                 break
-                        
+
                         case ('marketCapMin'):
                             if not (int(value) <= data['marketCap']):
                                 save = False
@@ -157,7 +169,7 @@ def main():
                             if not (float(value) >= data['profitMargins']):
                                 save = False
                                 break
-                        
+
                         case ('bookValueMin'):
                             if not (float(value) <= data['bookValue']):
                                 save = False
@@ -288,18 +300,52 @@ def main():
                                 save = False
                                 break
 
+            """
+            *********************** READ ME ****************************
+
+            What I'm trying to do below after we check all the user's preferences
+            and determine the stock should be saved is create a data frame with all
+            info we need to properly fill in the table, then adding it to the original
+            by using .concat() to stick "dfTemp" into what will be the final data frame
+            "validStocks" (validStocks is created in line 73).
+
+            What I have here has not been tested and we need to make sure that if we cannot
+            get a certain piece of data we fill in N/A or something instead of erroring
+            or skipping the stock (it has been determined valid so we can't just skip it
+            like we did in the screening process). Since where we are trying to make this
+            data frame is still inside of that try-catch I think we are going to run into
+            issues where stocks will be missing from the spreadsheet because one of these
+            data entries could not be found.
+
+            I added a new file in GitHub called dataFrameExample that you should look at.
+
+            NOTE: If you see something online saying use .append(), it's outdated and that
+            is no longer a thing. Concat() seemed like the way to go when I was looking
+            stuff up.
+
+            Good luck, soldier.
+
+            """
+
+            # If stock passes all preferences, save it
             if (save):
-                print("Saved!: ", data['symbol'])
-                
+                dfTemp = pd.DataFrame({'Ticker': [data['symbol']],
+                                       'Company Name': [data['longName']],
+                                       'Sector': [data['sector']],
+                                       'Industry': [data['industry']],
+                                       'Market Cap': [data['marketCap']],
+                                       'Price': [data['currentPrice']],
+                                       'P/E': [data['trailingPE']],
+                                       'Dividend Rate': [data['dividendRate']],
+                                       'Analyst Recommendation': [data['recommendationKey']]
+                                       })
+
         except KeyError:
             save = False
-            # Do nothing and go on to the next stock
-            
+            # Do nothing and screen the next stock, this one is missing data.
 
+# -----------------------------------------------------------------------------------------
 
-    # validStocks = pd.DataFrame(columns=['Ticker', 'Company Name'])
-    # Maybe download valid stocks from a list and put that info into excel
-    
-    
+# Run the program
 if __name__ == "__main__":
     main()
