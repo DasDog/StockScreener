243694ff5f47f652655dd8a85363fe2962052765
PsycheeBoi <94575520+PsycheeBoi@users.noreply.github.com>
2023-07-23 00:45:40 -0400

    Added dataHandler App and Context

--- stockScreener/home/views.py
+++ stockScreener/home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import pandas as pd
 from main import main
 
 
@@ -8,17 +7,6 @@ def landPage(request):
     context = {}
     return render(request, 'home/index.html', context)
 
-def dataPage(request):
-    
-    context = {}
-    data = main()
-    
-    for key in data:
-        
-        context[key] = data[key].values()
-        print(context[key], key)
-    
-    return render(request, 'home/dataView.html', context)
     
 
 
